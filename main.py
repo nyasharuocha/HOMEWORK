782eb044d134f1f19443d64595ee0ab4e23fdd02
nyasharuocha <57229496+nyasharuocha@users.noreply.github.com>
2021-07-11 06:18:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,64 @@
-import glob
-import os
-import shutil
-import glob
-import cv2
-import numpy as np
+"""Create an Image Classification Web App using PyTorch and Streamlit."""
+# import libraries
+from PIL import Image
+from torchvision import models, transforms
+import torch
 import streamlit as st
-import tempfile
-from keras.applications.vgg16 import VGG16
-from keras.preprocessing import image
-from matplotlib.pyplot import gray
-from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
 
-model = VGG16(
-    include_top=True,
-    weights="imagenet",
-    input_tensor=None,
-    input_shape=None,
-    pooling=None,
-    classes=1000,
-    classifier_activation="softmax",
-)
+# set title of app
+st.title("Simple Image Classification Application")
+st.write("")
 
-st.title("VIDEO MODEL USING VGG16")
-if not os.path.exists('frames'):
-    os.makedirs('frames')
-else:
-    shutil.rmtree('frames')
-    os.makedirs('frames')
-
-f = st.file_uploader("Upload a Video", type=['mp4'])
-count = 0
-
-if f is not None:
-    tfile = tempfile.NamedTemporaryFile(delete=False)
-    tfile.write(f.read())
-
-    vf = cv2.VideoCapture(tfile.name)
-
-    stframe = st.empty()
-
-    font_scale = 2
-    font = cv2.FONT_HERSHEY_PLAIN
-
-    while True:
-        ret, frame = vf.read()
-
-        if not ret:
-            break
-        name = './frames/*' + str(count) + '.jpg'
-        cv2.imwrite(name, frame)
-        img = './frames/*' + str(count) + '.jpg'
-        img = image.load_img(img, color_mode='rgb', target_size=(224, 224))
-        imageArray = image.img_to_array(img)
-        imageArray = np.expand_dims(imageArray, axis=0)
-        imageArray = preprocess_input(imageArray)
-        features = model.predict(imageArray)
-        img = cv2.putText(frame, str(decode_predictions(features)[0][0][1]), (30, 55), font, fontScale=font_scale,
-                          color=(0, 255, 0), thickness=4)
-        # img = cv2.imwrite('./frames/*' + str(count) + '.jpg', frame)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        stframe.image(img)
-        count += 1
+# enable users to upload images for the model to make predictions
+file_up = st.file_uploader("Upload an image", type = "jpg")
 
 
+def predict(image):
+    """Return top 5 predictions ranked by highest probability.
+    Parameters
+    ----------
+    :param image: uploaded image
+    :type image: jpg
+    :rtype: list
+    :return: top 5 predictions ranked by highest probability
+    """
+    # create a ResNet model
+    resnet = models.resnet101(pretrained = True)
+
+    # transform the input image through resizing, normalization
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean = [0.485, 0.456, 0.406],
+            std = [0.229, 0.224, 0.225]
+            )])
+
+    # load the image, pre-process it, and make predictions
+    img = Image.open(image)
+    batch_t = torch.unsqueeze(transform(img), 0)
+    resnet.eval()
+    out = resnet(batch_t)
+
+    with open('imagenet_classes.txt') as f:
+        classes = [line.strip() for line in f.readlines()]
+
+    # return the top 5 predictions ranked by highest probabilities
+    prob = torch.nn.functional.softmax(out, dim = 1)[0] * 100
+    _, indices = torch.sort(out, descending = True)
+    return [(classes[idx], prob[idx].item()) for idx in indices[0][:5]]
+
+
+if file_up is not None:
+    # display image that user uploaded
+    image = Image.open(file_up)
+    st.image(image, caption = 'Uploaded Image.', use_column_width = True)
+    st.write("")
+    st.write("Just a second ...")
+    labels = predict(file_up)
+
+    # print out the top 5 prediction labels with scores
+    for i in labels:
+        st.write("Prediction (index, name)", i[0], ",   Score: ", i[1])
 
